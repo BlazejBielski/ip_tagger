4556c4914ba24e3f7eb6ffe383704ef3c0fd65a6
Blazej Bielski <bbielski@lyck.pl>
2024-05-22 16:15:14 +0200

    docstring for model and serialzier, minor changes in docerfiles and .gitignore

--- api/iptags/models.py
+++ api/iptags/models.py
@@ -2,5 +2,15 @@ from django.db import models
 
 
 class IpTag(models.Model):
+    """Model for representing IP matched with tags.
+
+    :param tag: tag for IP address
+    :type tag: str
+    :param ip_network: IP address
+    :type ip_network: str
+    """
     tag = models.CharField(max_length=16)
     ip_network = models.GenericIPAddressField(unpack_ipv4=True)
+
+    def __str__(self):
+        return self.tag
--- api/iptags/serializers.py
+++ api/iptags/serializers.py
@@ -4,10 +4,20 @@ from .models import IpTag
 
 
 class IPTagSerializer(serializers.ModelSerializer):
+    """Serializer for IpTag model.
+
+    :returns: IPTagSerializer
+    :rtype: rest_framework.serializers.Serializer
+
+    """
     class Meta:
         model = IpTag
         fields = ['tag']
 
     def to_representation(self, instance):
+        """function for split tags
+        :param instance: serializer instance
+        :return: representation of serializer instance
+        """
         representation = super().to_representation(instance)
         return representation['tag'].split(", ")
