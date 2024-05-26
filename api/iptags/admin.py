4fb2958b26f01b85d51586ff71e6cf97074ae7b6
blazej <bbielski@lyck.pl>
2024-05-26 19:04:14 +0200

    change readme, add statcic, part of html and admin

--- api/iptags/admin.py
+++ api/iptags/admin.py
@@ -7,19 +7,19 @@ from iptags.models import IpTag
 User = get_user_model()
 
 
-class UserAdminConfig(UserAdmin):
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
-    list_filter = ('is_staff', 'is_active', 'date_joined')
-    ordering = ('-last_login',)
-    search_fields = ('email', 'username')
+# class UserAdminConfig(UserAdmin):
+#     list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+#     list_filter = ('is_staff', 'is_active', 'date_joined')
+#     ordering = ('-last_login',)
+#     search_fields = ('email', 'username')
 
 
 class IpTagAdminConfig(admin.ModelAdmin):
-    list_display = ('ip_address', 'tag')
-    list_filter = ('ip_address', 'tag')
-    ordering = ('ip_address',)
-    search_fields = ('ip_address', 'tag')
+    list_display = ('ip_network', 'tag')
+    list_filter = ('ip_network', 'tag')
+    ordering = ('ip_network',)
+    search_fields = ('ip_network', 'tag')
 
 
 admin.site.register(IpTag, IpTagAdminConfig)
-admin.site.register(User, UserAdminConfig)
+# admin.site.register(User, UserAdminConfig)
